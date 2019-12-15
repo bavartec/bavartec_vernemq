eb91e3cb34434e8bb90adc4ea963dd28403dfe81
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2019-12-16 00:00:00 +0100

    Go serverless

--- api/run.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-docker build -t api .
-docker run -t -i \
-  -v "/etc/letsencrypt:/etc/letsencrypt" \
-  -p 443:443 \
-  --name api api
--- db/ddl.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE vmq_auth_acl
-(
-  username VARCHAR(64) NOT NULL,
-  password BINARY(32) NOT NULL,
-  salt CHAR(32) NOT NULL,
-  CONSTRAINT vmq_auth_acl_primary_key PRIMARY KEY (username)
-);
